9c4cbeabd10bedffc34a44c202bf547ab22dacf6
VGalkin812 <trufell56@gmail.com>
2021-02-27 17:17:07 +0300

    Validating title length validation to title maximum-length validation to description custom validator for ends_at

--- apps/auction/lib/auction/item.ex
+++ apps/auction/lib/auction/item.ex
@@ -1,5 +1,6 @@
 defmodule Auction.Item do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "items" do
     field :title, :string
@@ -8,8 +9,19 @@ defmodule Auction.Item do
     timestamps()
   end
 
+  defp validate(:ends_at, ends_at_date) do
+    case DateTime.compare(ends_at_date, DateTime.utc_now()) do
+      :lt -> [ends_at: "ends_at cannot be in the past"]
+      _ -> []
+    end
+  end
+
   def changeset(item, params \\ %{}) do
     item
-    |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+    |> cast(params, [:title, :description, :ends_at])
+    |> validate_required(:title)
+    |> validate_length(:title, min: 3)
+    |> validate_length(:description, max: 200)
+    |> validate_change(:ends_at, &validate/2)
   end
 end
